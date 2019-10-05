17f7bbc283f3b37868a27ea65b2c4286d5bff014
user.name <hazem.hamed.h@hotmail.com>
2019-10-05 12:55:57 -0700

    added nwe form, updated server side

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 
 // var mysql = require("mysql");
-// var connection = mysql.createConnection(keys.data);
+// var connection = mysql.createConnection(process.env.JAWSDB_URL || keys.data);
 
 // var keys = require("./keys.js");
 // require("dotenv").config();
@@ -13,10 +13,14 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.listen(3000, function(){
-	console.log('listening on port 3000');
-})
+app.use(express.static("public"));
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/lib/index.html'));
+})
+
+app.listen( process.env.PORT || 3000, function(){
+	console.log('listening.....');
 })
